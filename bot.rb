492895668d5bd183200cc2d6c3b15e3284754f73
tbpgr <tbpgr@tbpgr.jp>
2014-12-14 03:36:26 +0900

    Add random tweet

--- bot.rb
+++ bot.rb
@@ -112,9 +112,47 @@ def anyone(robert_garcia, twitter_id, tweet)
   end
 end
 
+module RandomTweet
+  MESSAGES = [
+    "Tbpgr Slides |> 多言語ゴルフ場デスマコロシアム。デスマコロシアムとは？ |> http://tbpgr.github.io/deathma_slide/ #codeiq #デスマコロシアム",
+    "Tbpgr Qiita |> GitHub Flow 図解 |> http://qiita.com/tbpgr/items/4ff76ef35c4ff0ec8314",
+    "Tbpgr Qiita |> Ruby | アノテーションコメント（TODO、FIXME、OPTIMIZE、HACK、REVIEW） |> http://qiita.com/tbpgr/items/1c046a877c6be4d89876 #ruby",
+    "Tbpgr Qiita |> Ruby | Ruby の private と protected 。歴史と使い分け |> http://qiita.com/tbpgr/items/6f1c0c7b77218f74c63e #ruby",
+    "Tbpgr Qiita |> 条件分岐とループベースのロジックからコレクションパイプラインを利用したロジックへ |> http://qiita.com/tbpgr/items/190859b5080914896db8 #ruby",
+    "Tbpgr Qiita |> Ruby | RubyKaigi2014で話題に。安全に Ruby のコードを変換できる Synvert gem をインストール |> http://qiita.com/tbpgr/items/8bdb92fa8f9324727336 #ruby",
+    "Tbpgr Qiita |> Itamae 関連記事 |> https://qiita.com/tbpgr/items/8b0170341b8095ced543 #itamae",
+    "Tbpgr Qiita |> Gemfury 関連記事 |> https://qiita.com/tbpgr/items/a534dd2aa10995abc37a #gemfury",
+    "Tbpgr Qiita |> Docker 関連記事 |> https://qiita.com/tbpgr/items/f49ea1df791612aca94f #docker",
+    "Tbpgr Qiita |> Elixir 関連記事 |> https://qiita.com/tbpgr/items/a55ef9ea40200c5fd8ec #docker",
+    "Tbpgr Qiita |> Reveal.js 関連記事 |> https://qiita.com/tbpgr/items/dfe984e65323371fb9f0 #revealjs",
+    "Tbpgr Qiita |> Graphviz 関連記事 |> https://qiita.com/tbpgr/items/76d0379bb83ec64fbdb3 #graphviz",
+    "Tbpgr Qiita |> Wercker 関連記事 |> https://qiita.com/tbpgr/items/57ea0d1e1b15700a6ff3 #wercker",
+    "Tbpgr Qiita |> Kandan 関連記事 |> https://qiita.com/tbpgr/items/94609b53d083895b0d90 #kandan",
+    "Tbpgr Qiita |> Hubot 関連記事 |> https://qiita.com/tbpgr/items/544d397e2b6c17b24292 #hubot",
+    "Tbpgr Qiita |> Vagrant 関連記事 |> https://qiita.com/tbpgr/items/e97057f4bd01c23504b6 #vagrant",
+    "Tbpgr Qiita |> Ruboty 関連記事 |> https://qiita.com/tbpgr/items/f16c506c4a2636e95d34 #ruboty",
+    "Tbpgr Qiita |> GutLab CI 関連記事 |> https://qiita.com/tbpgr/items/e079210ac52822a33559 #gitlab",
+    "Tbpgr Qiita |> GutLab API 関連記事 |> https://qiita.com/tbpgr/items/4f301f8e7788cfbd5ace #gitlab",
+    "Tbpgr Qiita |> RuboCop API 関連記事 |> https://qiita.com/tbpgr/items/edbfecb6a6789dd54f47 #rubocop",
+    "Tbpgr gems |> ruboty plugin の README を生成する ruboty-megen gem |> https://github.com/tbpgr/ruboty-megen #ruboty",
+    "Tbpgr gems |> ruboty plugin の Qiita 記事を生成する ruboty-articlegen gem |> https://github.com/tbpgr/ruboty-articlegen #ruboty",
+    "Tbpgr gems |> 西暦から和暦を取得する ruboty plugin,  ruboty-wareki gem |> https://github.com/tbpgr/ruboty-wareki #ruboty",
+    "Tbpgr gems |> 西暦から十二支の名称、emoji を取得する ruboty plugin,  ruboty-eto gem |> https://github.com/tbpgr/ruboty-eto #ruboty",
+    "Tbpgr gems |> 西暦から十二支の名称、emoji を取得する eto gem |> https://github.com/tbpgr/eto #ruboty",
+    "Tbpgr gems |> 日付フォーマットのディレクトリ一括作成を行う defoker gem |> https://github.com/tbpgr/defoker #ruboty",
+    "Tbpgr Slides |> 日付フォーマットのディレクトリ一括作成を行う defoker gem スライドショー |> http://tbpgr.github.io/defoker_slide/ #defoker",
+  ]
+end
+
+def random_advertise(robert_garcia)
+  return if rand > 0.1
+  robert_garcia.tweet("#{RandomTweet::MESSAGES.sample} \n#{current_time}")
+end
+
 robert_garcia = RobertGarcia.new
 robert_garcia.stream_client.on_timeline_status do |status|
   begin
+    random_advertise(robert_garcia)
     twitter_id = status.user.screen_name
     tweet = status.text
     # TODO: そのうち使う管理者限定モード
